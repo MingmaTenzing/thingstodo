19a96e8b9022b92a0e234b1808f5d491005bc041
MingmaTenzing <pasangsherpa689@gmail.com>
2023-02-11 21:49:53 +0800

    landing page first section done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
 <div className=' flex items-center justify-between'>
        <div className='flex items-center space-x-4 '>
         <Image src={biglogo} alt="logo" className='w-[90px] md:w-[100px] ' />
-        <div className='md:flex hidden space-x-3'>
+        <div className='md:flex hidden space-x-3 text-sm'>
           <p>Home</p> 
           <p>Product</p> 
           <p>Pricing</p> 
@@ -14,8 +14,8 @@ function Nav() {
            </div>
            </div>
            <div className='flex space-x-2'>
-           <button className='border py-1 px-2 md:py-2 md:px-3 rounded-lg'>Log in</button>
-           <button className='border py-1 px-2  md:py-2 md:px-3 rounded-lg bg-black text-white'>Sign up</button>
+           <button className='border py-1 px-2 md:py-2 md:px-3 rounded-lg text-sm'>Log in</button>
+           <button className='border py-1 px-2  md:py-2 md:px-3 rounded-lg text-sm bg-black text-white'>Sign up</button>
       </div>
       </div>
   )
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import {Inter} from '@next/font/google'
+
+const roboto = Inter({subsets: ['latin'],
+weight:['100', '300', '200', '400', '500', '600', '700', '800', '900']})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <main className={roboto.className}>
+    <Component {...pageProps} />
+  </main>
+  )
 }
