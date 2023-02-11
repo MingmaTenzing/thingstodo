085798adcfff866c9717de2d616db0eaa26a8bd3
MingmaTenzing <pasangsherpa689@gmail.com>
2023-02-11 20:55:43 +0800

    added assest folder aswell

---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import biglogo from "../assests/logoxl.png"
+function Nav() {
+  return (
+<div className=' flex items-center justify-between'>
+       <div className='flex items-center space-x-4 '>
+        <Image src={biglogo} alt="logo" className='w-[90px] md:w-[100px] ' />
+        <div className='md:flex hidden space-x-3'>
+          <p>Home</p> 
+          <p>Product</p> 
+          <p>Pricing</p> 
+          <p>Resources</p> 
+          <p>Contact Us</p> 
+           </div>
+           </div>
+           <div className='flex space-x-2'>
+           <button className='border py-1 px-2 md:py-2 md:px-3 rounded-lg'>Log in</button>
+           <button className='border py-1 px-2  md:py-2 md:px-3 rounded-lg bg-black text-white'>Sign up</button>
+      </div>
+      </div>
+  )
+}
+export default Nav
