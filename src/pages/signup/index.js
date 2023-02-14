f47caa08785f1d8f2c8b620d3370c4d509c7c7af
MingmaTenzing <pasangsherpa689@gmail.com>
2023-02-14 14:30:17 +0800

    added loading state for signup page

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,52 +1,49 @@
 import Image from "next/image";
 import { useState } from "react";
 import logo from "../../assests/logoxl.png";
+import { ScaleLoader } from "react-spinners";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/firebase/init";
 import { Router, useRouter } from "next/router";
+import { blueGrey } from "@mui/material/colors";
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmpassword, setconfirmPassword] = useState("");
   const [borderRed, setBorderRed] = useState(false);
-  const [user , setUser] = useState()
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(false);
 
   const router = useRouter();
 
-
   function registerUser(event) {
+    setLoading(true);
+
     event.preventDefault();
+
     if (password !== confirmpassword) {
       setBorderRed(true);
     } else {
-        
-      createUserWithEmailAndPassword( auth, email, confirmpassword).then ((userCredential) => {
-       setUser(userCredential.user);
-       router.push('/')
-        
-      }).catch ((error) => {
-        const erroCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage)
-      })
+      createUserWithEmailAndPassword(auth, email, confirmpassword)
+        .then((userCredential) => {
+          setUser(userCredential.user);
+          setLoading(false);
+
+          router.push("/");
+        })
+        .catch((error) => {
+          const erroCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorMessage);
+        });
     }
-
-
-   
-  
-
-
   }
-  
-
-
-
 
   return (
     <div>
       <div className="flex flex-col items-center pt-[100px]">
         <div className="flex flex-col items-center space-y-5 mb-5 ">
-          <Image src={logo} alt="thingstodo logo" width={200} height={200}  />
+          <Image src={logo} alt="thingstodo logo" width={200} height={200} />
           <h1 className="font-bold text-lg ">
             {" "}
             Create your <span className="text-thingstodo">Thingstodo</span>{" "}
@@ -59,12 +56,12 @@ function Signup() {
         >
           <div>
             <h1 className="text-sm"> Enter your email Address</h1>
-            <input required 
+            <input
+              required
               type="email"
               placeholder="email@gmail.com"
               className="border px-5 py-2 text-sm outline-none rounded-lg"
               onChange={(event) => setEmail(event.target.value)}
-
             ></input>
           </div>
 
@@ -73,7 +70,8 @@ function Signup() {
               {" "}
               <div>
                 <h1 className="text-sm"> Enter your password </h1>
-                <input required
+                <input
+                  required
                   type="text"
                   onChange={(e) => setPassword(e.target.value)}
                   className="border border-red-600 outline-none px-5 py-2 text-sm rounded-lg"
@@ -81,7 +79,8 @@ function Signup() {
               </div>
               <div>
                 <h1 className="text-sm"> Confirm your password </h1>
-                <input required
+                <input
+                  required
                   type="text"
                   onChange={(e) => setconfirmPassword(e.target.value)}
                   className=" border border-red-600 outline-none px-5 py-2 text-sm rounded-lg"
@@ -92,7 +91,8 @@ function Signup() {
             <>
               <div>
                 <h1 className="text-sm"> Enter your password </h1>
-                <input required
+                <input
+                  required
                   type="text"
                   onChange={(e) => setPassword(e.target.value)}
                   className="border  border-gray-200 outline-none px-5 py-2 text-sm rounded-lg"
@@ -100,7 +100,8 @@ function Signup() {
               </div>
               <div>
                 <h1 className="text-sm"> Confirm your password </h1>
-                <input required
+                <input
+                  required
                   type="text"
                   onChange={(e) => setconfirmPassword(e.target.value)}
                   className="border border-gray-200 outline-none px-5 py-2 text-sm rounded-lg"
@@ -108,13 +109,15 @@ function Signup() {
               </div>
             </>
           )}
-
-          <button
-            type="submit"
-            className="border py-2 px-4 bg-thingstodo text-white rounded-lg"
-          >
-            Sign up
-          </button>
+          <div className="flex space-x-2 items-center">
+            <button
+              type="submit"
+              className="border py-2 px-4 bg-thingstodo text-white rounded-lg"
+            >
+              Sign up
+            </button>
+           {loading? <> <ScaleLoader color="#5271ff" height={30} /></> : null} 
+          </div>
         </form>
       </div>
     </div>
