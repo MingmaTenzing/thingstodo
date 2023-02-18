0a7253011fff9099a931ba758f26102cca935a3b
MingmaTenzing <pasangsherpa689@gmail.com>
2023-02-18 18:13:22 +0800

    design add task page

--- src/components/BottomNavigationBar.js
+++ src/components/BottomNavigationBar.js
@@ -3,11 +3,11 @@ import { ClockIcon, PlusIcon, CheckCircleIcon } from "@heroicons/react/24/outlin
 
 function BottomNavigationBar() {
   return (
-    <div className="fixed bottom-0 w-full p-2 bg-footer text-white">
+    <div className="fixed bottom-0 w-full p-2 bg-footer text-white ">
         <div className=" flex justify-around items-center">
-            <ClockIcon className="w-10 " />
-            <PlusIcon  className="w-10" />
-            <CheckCircleIcon className="w-10 " />
+            <ClockIcon className="w-10 text-orange-400 " />
+            <PlusIcon  className="w-10  text-thingstodo" />
+            <CheckCircleIcon className="w-10  text-green-400" />
              
         </div>
     </div>
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -2,46 +2,66 @@ import BottomNavigationBar from "@/components/BottomNavigationBar";
 import Nav from "@/components/Nav";
 import { auth } from "@/firebase/init";
 import { onAuthStateChanged } from "firebase/auth";
+import Image from "next/image";
 import { useEffect, useState } from "react";
+import addtaskimg from "../../assests/addtasks.svg";
 
-function dashBoard () {
-    const [user, setUser] = useState();
+function dashBoard() {
+  const [user, setUser] = useState();
 
-    useEffect (() => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-              setUser(user);
-              console.log(user)
-            }
-          })
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        console.log(user);
+      }
+    });
+  }, [user]);
 
-    }, [user])
-
-
-   
   return (
-    <div>
-       
-        <Nav user={user} />
-        
-        
+    <div className="">
+      <Nav user={user} />
 
-        <main className="mt-10">
-            <h1 className="text-lg text-center "> Add the tasks you want <span> to do  </span> </h1>
-            <form  className="pl-10 mt-10 space-y-5">
-                <div> 
-                <h1 className="text-sm">Task Title</h1>
-                <input type="text" className="border w-[300px] h-10 rounded-lg"></input>
-                </div>
+      <main className="space-y-8 mt-10 w-2/3 p-5 shadow-lg border m-auto rounded-3xl bg-slate-100 md:w-[900px] md:flex md:mt-20" >
 
-                <div> 
-                <h1 className="text-sm">Description</h1>
-                <input type="text" className="border w-[300px] h-[100px] rounded-lg"></input>
-                </div>
-                </form>
-        </main>
-        <BottomNavigationBar />
+     
+
+        <div className="flex justify-center">
+          <Image src={addtaskimg} alt="addtask" width={100} height={100} className='w-[250px] md:flex md:w-[500px] '/>
+        </div>
+
+        <div>
+          <h1 className="  text-[20px] text-center font-[900] sm:text-[25px] ">
+            {" "}
+            Add the <span className="text-thingstodo">things</span>  you want{" "}
+            <span className="text-thingstodo"> todo </span>{" "}
+          </h1>
+
+          <form className=" mt-10 space-y-5 flex flex-col items-center">
+            <div>
+              <h1 className="text-sm">Task Title</h1>
+              <input
+                type="text"
+                className="border  outline-none w-[300px] p-2 h-10 rounded-lg"
+              ></input>
+            </div>
+
+            <div>
+              <h1 className="text-sm">Description</h1>
+              <input
+                type="text"
+                className="border  outline-none p-2 w-[300px] h-[100px] rounded-lg"
+              ></input>
+            </div>
+            <button className="p-3 bg-thingstodo rounded-lg text-white">
+              {" "}
+              Add Task{" "}
+            </button>
+          </form>
+        </div>
+      </main>
+      <BottomNavigationBar />
     </div>
-  )
+  );
 }
 export default dashBoard;
